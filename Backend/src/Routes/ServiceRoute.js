83a32e4907f5a1871d29816c05e2d9f313808469
Piyush Gupta <126002609+piyush-creations@users.noreply.github.com>
2026-04-15 15:26:23 +0530

    Fix import case for verifyAdmin middleware

--- Backend/src/Routes/ServiceRoute.js
+++ Backend/src/Routes/ServiceRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import verifyAdmin from "../Middlewares/VerifyAdmin.js";
+import verifyAdmin from "../Middlewares/verifyAdmin.js";
 
 const router = express.Router();
 
