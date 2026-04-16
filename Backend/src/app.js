7fa96bb3e6745e74eaa262b4c8173ca35bae55d3
Piyush <puyeshj110@gmail.com>
2026-04-16 20:19:54 +0530

    domai changed again

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-  origin: "https://bridesmakeupmnatra.vercel.app",
+  origin: "https://bridesmakeupmantra.vercel.app",
   credentials: true
 }));
 
